e3f35403c2ab73b4e489c64c7b99912cde6c4129
xStFtx <you@example.com>
2023-10-11 10:20:05 +0200

    2.4

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import psutil
 import pyshark
 import subprocess
 import logging
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QTextEdit, QLabel, QComboBox, QProgressBar, QInputDialog, QErrorMessage, QListWidget, QCheckBox, QLineEdit
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QPushButton, QTextEdit, QLabel, QComboBox,
+                             QProgressBar, QInputDialog, QListWidget, QCheckBox, QLineEdit, QHBoxLayout, QErrorMessage)
+
 from PyQt5.QtCore import QThread, pyqtSignal
 import asyncio
 import ipaddress
@@ -13,6 +15,7 @@ logging.basicConfig(level=logging.DEBUG)
 DEFAULT_SCAN_DURATION = 10
 MIN_SCAN_DURATION = 5
 MAX_SCAN_DURATION = 120
+
 
 class IPScanner(QThread):
     signal = pyqtSignal('PyQt_PyObject', 'QString')
@@ -28,9 +31,14 @@ class IPScanner(QThread):
     def run(self):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
+        self.progress_signal.emit("Detecting processes...")
         pid = self.detect_process()
-        ips = self.capture_traffic(pid) if pid else []
-        self.scan_with_nmap(ips)
+        if pid:
+            self.progress_signal.emit(f"Capturing traffic for PID {pid}...")
+            ips = self.capture_traffic(pid)
+            self.scan_with_nmap(ips)
+        else:
+            self.signal.emit(set(), "Process not found!")
 
     def detect_process(self):
         for proc in psutil.process_iter(attrs=["pid", "name"]):
@@ -57,16 +65,28 @@ class IPScanner(QThread):
             result = NmapScanner.scan_ip(ip)
             self.signal.emit(set([ip]), result)
 
+
 class NmapScanner:
     @staticmethod
-    def scan_ip(ip):
+    def scan_ip(ip, scan_type='-sV', skip_host_discovery=False):
         try:
-            cmd_scan = ["nmap", "-sV", ip]
+            cmd_scan = ["nmap", scan_type]
+            if skip_host_discovery:
+                cmd_scan.append('-Pn')
+            cmd_scan.append(ip)
             result = subprocess.check_output(cmd_scan, stderr=subprocess.STDOUT, text=True)
             return result
         except subprocess.CalledProcessError as e:
             logging.error(e.output)
             return f"Error scanning {ip}: {e.output}"
+
+
+
+def get_primary_interface():
+    net_stats = psutil.net_io_counters(pernic=True)
+    primary_interface = max(net_stats, key=lambda x: (net_stats[x].bytes_sent + net_stats[x].bytes_recv))
+    return primary_interface
+
 
 class App(QWidget):
     def __init__(self):
@@ -76,31 +96,42 @@ class App(QWidget):
     def initUI(self):
         layout = QVBoxLayout()
 
-        self.statusLabel = QLabel("Game Status: Not Detected")
-        layout.addWidget(self.statusLabel)
+        self.logEdit = QTextEdit()
+        self.logEdit.setReadOnly(True)
+        layout.addWidget(self.logEdit)
 
+        processLayout = QVBoxLayout()
         self.processSearchBar = QLineEdit()
         self.processSearchBar.setPlaceholderText("Search processes...")
         self.processSearchBar.textChanged.connect(self.filter_processes)
-        layout.addWidget(self.processSearchBar)
+        processLayout.addWidget(self.processSearchBar)
 
         self.refreshButton = QPushButton("Refresh Processes")
         self.refreshButton.clicked.connect(self.refresh_processes)
-        layout.addWidget(self.refreshButton)
+        processLayout.addWidget(self.refreshButton)
 
         self.processComboBox = QComboBox()
-        layout.addWidget(self.processComboBox)
+        processLayout.addWidget(self.processComboBox)
+
+        layout.addLayout(processLayout)
 
         self.interfaceComboBox = QComboBox()
-        self.interfaceComboBox.addItems(self.get_windows_interfaces())
+        layout.addWidget(QLabel("Select Interface:"))
+        interfaces = self.get_windows_interfaces()
+        self.interfaceComboBox.addItems(interfaces)
         layout.addWidget(self.interfaceComboBox)
 
         self.filterLocalIpsCheckbox = QCheckBox("Filter Local IPs")
         self.filterLocalIpsCheckbox.setChecked(True)
         layout.addWidget(self.filterLocalIpsCheckbox)
 
-        self.ipListView = QListWidget()
-        layout.addWidget(self.ipListView)
+
+        self.scanTypeComboBox = QComboBox()
+        self.scanTypeComboBox.addItems(["-sV (Version detection)", "-sP (Ping scan)"])
+        layout.addWidget(QLabel("Nmap Scan Type:"))
+        layout.addWidget(self.scanTypeComboBox)
+        self.nmapPnCheckbox = QCheckBox("-Pn (Skip host discovery)")
+        layout.addWidget(self.nmapPnCheckbox)
 
         self.scanButton = QPushButton("Scan IPs")
         self.scanButton.clicked.connect(self.on_scan)
@@ -122,13 +153,11 @@ class App(QWidget):
             network_processes = []
         processes_info = psutil.process_iter(attrs=["name", "pid"])
         processes_list = [proc.info for proc in processes_info]
-        processes = {proc['name'] for proc in processes_list if proc['pid'] in network_processes}
+        processes = {f"{proc['name']} ({proc['pid']})": proc['pid'] for proc in processes_list if proc['pid'] in network_processes}
         self.processComboBox.clear()
-        self.processComboBox.addItems(sorted(processes))
-        if not processes:
-            self.statusLabel.setText("Status: No network processes found.")
-        else:
-            self.statusLabel.setText("Status: Processes refreshed.")
+        for name, pid in sorted(processes.items()):
+            self.processComboBox.addItem(name, pid)
+        self.logEdit.append("Processes refreshed.")
 
     def filter_processes(self):
         search_text = self.processSearchBar.text().lower()
@@ -140,16 +169,26 @@ class App(QWidget):
         try:
             tshark_if_output = subprocess.check_output(["tshark", "-D"])
             lines = tshark_if_output.decode("utf-8").strip().split("\n")
-            return [line[line.find("(") + 1:line.find(")")] for line in lines]
+            interfaces = [line[line.find("(") + 1:line.find(")")] for line in lines]
+
+            primary_interface = get_primary_interface()
+            if primary_interface in interfaces:
+                interfaces.remove(primary_interface)
+                interfaces.insert(0, primary_interface) 
+
+            return interfaces
         except FileNotFoundError:
             error_msg = QErrorMessage(self)
             error_msg.showMessage("tshark is not found. Please ensure Wireshark is installed.")
             return []
 
     def on_scan(self):
-        process = self.processComboBox.currentText()
+        process = self.processComboBox.currentData()  
         interface = self.interfaceComboBox.currentText()
         filter_local_ips = self.filterLocalIpsCheckbox.isChecked()
+        skip_host_discovery = self.nmapPnCheckbox.isChecked()
+        scan_type = self.scanTypeComboBox.currentText().split()[0] 
+
         duration, ok = QInputDialog.getInt(self, 'Set Duration', 'Enter scan duration (seconds):', value=DEFAULT_SCAN_DURATION, min=MIN_SCAN_DURATION, max=MAX_SCAN_DURATION)
         if not ok:
             return
@@ -158,20 +197,21 @@ class App(QWidget):
         self.scanner.progress_signal.connect(self.update_progress)
         self.progressBar.setMaximum(0)
         self.progressBar.setMinimum(0)
-        self.ipListView.clear()
+        self.logEdit.clear()
         self.scanner.start()
 
-    def update_ips(self, ips, nmap_output):
-        if ips:
-            ip = list(ips)[0]
-            self.ipListView.addItem(f"Results for {ip}:\n{nmap_output}")
-            self.progressBar.setMaximum(1)
-            self.progressBar.setValue(1)
-        else:
-            self.statusLabel.setText("Status: Not Detected")
+
+    def update_ips(self, ips, message):
+        for ip in ips:
+            self.logEdit.append(f"Detected IP: {ip}")
+        if message:
+            self.logEdit.append(message)
+        self.progressBar.setMaximum(100)
+        self.progressBar.setValue(100)
 
     def update_progress(self, message):
-        self.statusLabel.setText(f"Status: {message}")
+        self.logEdit.append(message)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
